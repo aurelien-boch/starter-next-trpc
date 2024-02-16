7180a163559d8e317b4dc2b82a83b92151ff3bb9
Aurélien BOCH <aurelien.boch@epitech.eu>
2024-02-16 11:54:26 +0100

    fix styling

--- api/src/infrastructure/persistence/demo/queries/postgres/save.ts
+++ api/src/infrastructure/persistence/demo/queries/postgres/save.ts
@@ -7,7 +7,7 @@ const saveQuery = `
             created_at,
             updated_at,
             deleted_at
-            )
+        )
     values
         (
             $1,
@@ -15,7 +15,7 @@ const saveQuery = `
             $3,
             $4,
             $5
-            )
+        )
         on conflict (id) do update set
             (title, updated_at, deleted_at) = ($2, $4, $5);
 `;
